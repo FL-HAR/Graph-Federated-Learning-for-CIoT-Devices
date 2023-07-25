b50bccda16686940415ba0a4af02eefbeaeaa38f
Arash Rasti <42555780+arashrasti96@users.noreply.github.com>
2023-07-24 21:29:05 -0400

    Update eval.py

--- utils/eval.py
+++ utils/eval.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-def evaluate_manual_data(ww,data_test,label_test,keras_model,client_id):
+def evaluate_personalization(ww,data_test,label_test,keras_model,client_id):
 
   keras_model.compile(
       loss=tf.keras.losses.SparseCategoricalCrossentropy(),
@@ -11,13 +11,13 @@ def evaluate_manual_data(ww,data_test,label_test,keras_model,client_id):
   client_ydata = label_test[client_id]
   return keras_model.evaluate(client_xdata,client_ydata)
 
-def evaluate(client_weights,dataset_test,keras_model,client_id):
+def evaluate_generalization(client_weights,dataset_test,keras_model):
   
   keras_model.compile(
       loss=tf.keras.losses.SparseCategoricalCrossentropy(),
       metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]  
   )
   keras_model.set_weights(client_weights)
-  client_xdata = np.expand_dims(dataset_test[client_id][0],-1)
-  client_ydata = dataset_test[client_id][1]
+  client_xdata = np.expand_dims(dataset_test[0],axis=-1)
+  client_ydata = dataset_test[1]
   return keras_model.evaluate(client_xdata,client_ydata)
